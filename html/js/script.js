2f73101ec79a5f767292a8031cbdc6d0b266ff44
Lewis Danks <lewdanks@gmail.com>
2022-02-03 19:08:52 +0000

    changed js conditions to allow for left and right movement

--- html/js/script.js
+++ html/js/script.js
@@ -1,13 +1,20 @@
 function moveCard(e)  {
     e = e || window.event;
     var target = e.target || e.srcElement
-    if(target.className.includes("card")){
-        if(target.style.animationPlayState != 'running') {
-            target.style.animationPlayState = 'running';
-        }
-        else {
-            target.style.animation = 'moveLeft 1s ease-out forwards'
-            target.style.animationPlayState = 'running';
+    if(target.className === 'card'){
+        if (target.className.includes("card")) {
+            if (target.style.animation.includes('moveRight') && target.style.animationPlayState != 'running') {
+                target.style.animationPlayState = 'running';
+            }
+            else if (target.style.animation.includes('moveLeft') && target.style.animationPlayState != 'running') {
+                target.style.animationPlayState = 'running';
+            }
+            else if (target.style.animation.includes('moveRight') && target.style.animationPlayState === 'running') {
+                target.style.animation = 'moveLeft 1s ease-out forwards';
+            }
+            else {
+                target.style.animation = 'moveRight 1s ease-out forwards';
+            }
         }
     }
 }
